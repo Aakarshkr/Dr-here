43ef57905178b911ec10f714b3bdd1088f0e5618
Aakarshkr <akarshkr88@gmail.com>
2024-12-21 10:00:28 +0530

    first commit frontend

---
+++ frontend/src/Components/TopDoctors.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const TopDoctors = () => {
+  return (
+    <div>
+      <h1>Top Doctors</h1>
+      <p>Our Top Doctors</p>
+      <div>
+        
+      </div>
+    </div>
+  )
+}
+
+export default TopDoctors
